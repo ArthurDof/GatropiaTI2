c07042b6fcd2ae9f7c2ad406a1682d222f4d50c1
antiiiinio <roantonioaraujo@gmail.com>
2025-09-22 13:51:10 -0300

    SFX colocados
    efeitos sonoros de menu e interface aplicados e efeitos sonoros de feedback colocados nos arquivos

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public AudioSource player;
+    public AudioClip[] audios;
+    public AudioMixer mixer;
 
-    // Update is called once per frame
-    void Update()
+    public Slider VolSlider;
+
+    public void PlayerAudio(int i)
     {
-        
+        player.clip = audios[i];
+        player.Play();
+    }
+    public void MudarVolume(int i)
+    {
+        if (i == 0)
+        {
+            mixer.SetFloat("SFXvol", VolSlider.value);
+        }
+
+    }
+    public void SalvarVolume()
+    {
+        PlayerPrefs.SetFloat("SFXvol", VolSlider.value);
+        PlayerPrefs.Save();
     }
 }
