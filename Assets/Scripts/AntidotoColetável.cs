9c6c68ddfeee404b5fa567d874aa3da6f90ba6f1
Mari-Berdine <62489521+Mari-Berdine@users.noreply.github.com>
2025-10-19 22:35:11 -0300

    consertei coletavel e ajustei canvas

---
+++ Assets/Scripts/AntidotoColetável.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class AntidotoColetavel : MonoBehaviour
+{
+    private GameManager gm;
+    private AudioController sfx;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+        sfx = GameObject.FindGameObjectWithTag("GameController").GetComponent<AudioController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            int rndSFX = Random.Range(1, 9);
+            sfx.PlayerAudio(rndSFX);
+
+            // ((slider de detecção)
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,7 @@ public class GameManager : MonoBehaviour
     public float tempomax= 100f;
     public float tempofaltando;
     public Slider Tempo;
+    public TextMeshProUGUI tempoTexto;
 
     //pontuação (base em colisões)
     int batidas;
@@ -25,6 +26,7 @@ public class GameManager : MonoBehaviour
     double pontos = 0;
     double pontosFinais=0;
     public TextMeshProUGUI pontosVitoria;
+    public TextMeshProUGUI pontosDuranteOJogo;
 
     void Start()
     {
@@ -38,6 +40,8 @@ public class GameManager : MonoBehaviour
     {
         tempofaltando -= Time.deltaTime;
         Tempo.value = tempofaltando;
+        tempoTexto.text = tempofaltando.ToString("F1");
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (vitoriaderrota == 0)
@@ -61,6 +65,15 @@ public class GameManager : MonoBehaviour
         {
             pontos = 1000;
         }
+        if (tempofaltando <= 10)
+        {
+            tempoTexto.color = Color.red;
+        }
+        else
+        {
+            tempoTexto.color = Color.white;
+        }
+        pontosDuranteOJogo.text = $"{pontos:F0}";
     }
 
 
